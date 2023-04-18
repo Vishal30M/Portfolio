aeac0b62908317ec0fa2e1bb887d4409581e1f90
Vishal <vismiss.30@gmail.com>
2023-04-18 22:41:01 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector('.burger');
+const menuMobile = document.querySelector('.menu-mobile');
+const closeIcon = document.querySelector('.close-icon');
+
+burger.addEventListener('click', () => {
+  menuMobile.classList.add('active');
+});
+
+closeIcon.addEventListener('click', () => {
+  menuMobile.classList.remove('active');
+});
+
+
+
+
+
+
+
