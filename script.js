00d0368807380fb38d6ed6243118595067798507
hafiz1379 <hafizrasa1379@gmail.com>
2023-04-20 20:09:53 +0430

    Added new object to script file

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ Window.addEventListener('resize', () => {
     menuMobile.classList.remove('active');
   }
 });
+
+const projects = [
+  {
+    title: "Tonic",
+    imgSrc: "images/SS0.png",
+    imgAlt: "snapshot",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    details: {
+      company: "CANOPY",
+      role: "Back End Dev",
+      year: "2015",
+    },
+    languages: ["HTML", "CSS", "Javascript"],
+    btnText: "See Project",
+  },
+  {
+    title: "Tonic",
+    imgSrc: "images/SS2.png",
+    imgAlt: "snapshot-1",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    details: {
+      company: "CANOPY",
+      role: "Back End Dev",
+      year: "2015",
+    },
+    languages: ["HTML", "CSS", "Javascript"],
+    btnText: "See Project",
+  },
+];
