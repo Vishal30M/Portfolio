2610fceb4b896e67f3fdb712ccefc44db919099d
Vishal <vismiss.30@gmail.com>
2023-04-21 22:40:03 +0530

    Add form validation

--- script.js
+++ script.js
@@ -305,18 +305,16 @@ buttons.forEach((button) => button.addEventListener('click', showModal));
 
 // form-validation//
 
+const form = document.querySelector('form');
+const emailInput = document.getElementById('email');
+const submitButton = document.querySelector('.submit');
 
-  const form = document.querySelector('form');
-  const emailInput = document.getElementById('email');
-  const submitButton = document.querySelector('.submit');
-
-  form.addEventListener('submit', function (event) {
-    if (emailInput.value !== emailInput.value.toLowerCase()) {
-      event.preventDefault();
-      const errorText = document.createElement('p');
-      errorText.textContent = 'Email should be in lowercase.';
-      errorText.style.color = 'red';
-      submitButton.parentNode.insertBefore(errorText, submitButton);
-    }
-  });
-
+form.addEventListener('submit', (event) => {
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    event.preventDefault();
+    const errorText = document.createElement('p');
+    errorText.textContent = 'Email should be in lowercase.';
+    errorText.style.color = 'red';
+    submitButton.parentNode.insertBefore(errorText, submitButton);
+  }
+});
